d8eca23fc753f63b83346bc5ebfbc39c26c623ff
Maarten Moens <Maarten.Moens@UGent.be>
2018-03-21 19:21:55 +0100

    First template

--- App/app.py
+++ App/app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+@app.route("/<user>")
+def show_stats(user):
+    return render_template('test.html', user=user)
